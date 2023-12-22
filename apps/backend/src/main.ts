5fc314c77a765151c2f08a862aa19840a3539dec
Withney Guilherme <wguilhermecriativo@gmail.com>
2023-12-22 15:59:46 -0300

    chore: setup nestjs monorepo with backend and frontend subapps and activate monorepo mode

--- apps/backend/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { BackendModule } from './backend.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(BackendModule);
-  await app.listen(3000);
-}
-bootstrap();
